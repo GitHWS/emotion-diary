dd03de9428411d6086acd33ce974dc872a318960
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-04-24 15:33:27 +0900

    MyButton 컴포넌트 type prop 예외처리 로직 추가

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const MyButton = ({ text, type = 'default', onClick }) => {
+  const btnType = ['positive', 'negative'].includes(type) ? type : 'default';
+
   return (
     <button
       onClick={onClick}
-      className={['MyButton', `MyButton_${type}`].join(' ')}>
+      className={['MyButton', `MyButton_${btnType}`].join(' ')}>
       {text}
     </button>
   );
