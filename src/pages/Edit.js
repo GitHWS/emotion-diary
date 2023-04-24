cd2d5dec885d014e1bba2bbc70b9f21ee48678c9
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-04-24 14:49:38 +0900

    Params 받아오기, 쿼리 스트링 데이터 가져오기, 페이지 이동 기능 추가

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParmas, setSearchParams] = useSearchParams();
+
+  const id = searchParmas.get('id');
+  const mode = searchParmas.get('mode');
+  console.log(id, mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이 곳은 일기 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: 'Winterlood' })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate('/home');
+        }}>
+        Home으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}>
+        뒤로가기
+      </button>
     </div>
   );
 };
