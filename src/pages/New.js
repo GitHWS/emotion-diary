66001c124427fb77055e55859f707891be51d763
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-05-02 21:12:07 +0900

    New 페이지 날짜 선택 input 기능 구현

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import MyHeader from '../components/MyHeader';
+import MyButton from '../components/MyButton';
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
 
 const New = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>New</h1>
-      <p>이 곳은 일기 작성 페이지입니다.</p>
+      <MyHeader
+        headText="새 일기쓰기"
+        leftChild={<MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />}
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
