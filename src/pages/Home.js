23038cf8a7bdf11b7d5d5324463089f9f085d1db
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-05-03 20:00:31 +0900

    일기 삭제 후 Home 페이지 타이틀이 Edit 페이지 타이틀으로 적용되는 에러 해결

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,11 @@ const Home = () => {
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
 
   useEffect(() => {
+    let titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `감정 일기장`;
+  }, []);
+
+  useEffect(() => {
     if (diaryList.length >= 1) {
       const firstDay = new Date(
         curDate.getFullYear(),
@@ -36,10 +41,6 @@ const Home = () => {
       );
     }
   }, [diaryList, curDate]);
-
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
 
   const increaseMonth = () =>
     setCurDate(
